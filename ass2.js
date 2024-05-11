803db37b80bff75d475abf2f7e06e9582ec8734d
Nikhilsangale2002 <145535157+Nikhilsangale2002@users.noreply.github.com>
2024-05-11 19:44:14 +0530

    Create ass2.js

---
+++ ass2.js
@@ -0,0 +1,37 @@
+function foo() {
+    x = 1;
+    function x() { }
+    var x = '2';
+    return x;
+}
+alert( typeof foo() );
+
+
+
+Output:
+        string.
+
+function foo() {
+    return x;
+    x = 1;
+    
+    function x() { }
+    
+    var x = '2';
+}
+console.log( typeof foo() ); 
+
+Output:
+        Function.
+
+var x = 1;
+function foo() {
+    x = 10;
+    return;
+    function x() {}
+}
+foo();
+console.log( x );
+
+Output:
+        x=1.
